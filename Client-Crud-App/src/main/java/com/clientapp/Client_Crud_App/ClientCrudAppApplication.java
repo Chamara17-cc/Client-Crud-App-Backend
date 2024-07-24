233eb3f765c669606eb9cf880a89d9f4407aa054
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-07-25 00:44:07 +0530

    Project

---
+++ Client-Crud-App/src/main/java/com/clientapp/Client_Crud_App/ClientCrudAppApplication.java
@@ -0,0 +1,13 @@
+package com.clientapp.Client_Crud_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientCrudAppApplication.class, args);
+	}
+
+}
