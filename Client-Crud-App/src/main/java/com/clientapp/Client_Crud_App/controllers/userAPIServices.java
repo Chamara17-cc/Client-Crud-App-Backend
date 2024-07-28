2e52894fa3c53ec82024775c4ed6183d9cb132ef
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-07-28 22:59:23 +0530

    edit and delete

--- Client-Crud-App/src/main/java/com/clientapp/Client_Crud_App/controllers/userAPIServices.java
+++ Client-Crud-App/src/main/java/com/clientapp/Client_Crud_App/controllers/userAPIServices.java
@@ -1,8 +1,10 @@
 package com.clientapp.Client_Crud_App.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,23 @@ public class userAPIServices {
     {
        this.user=user;
        return "User created sucessfullly";
+    }
+
+    @PutMapping
+    public String updateUser(@RequestBody User user)
+    {
+       this.user=user;
+       return "User updated sucessfullly";
+    }
+
+    @DeleteMapping("{userid}")
+    public String deleteUser(@PathVariable("userid") Integer Id){
+
+        this.user=null;
+        return "Deleted succsussfully";
+    }
+    @GetMapping
+    public User getAllUsers(){
+        return user;
     }
 }
